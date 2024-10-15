f970d4cc4d124fcee2b3985f3b68651f720dc807
Guisbeghendev <guisbeghendev@gmail.com>
2024-10-15 17:42:22 -0300

    v2-att24- perfil ok

--- profilegs/views.py
+++ profilegs/views.py
@@ -1,13 +1,9 @@
-# profilegs/views.py
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
 
 @login_required
 def profilegs_view(request):
     user = request.user  # Recupera o usuário autenticado
-    try:
-        return render(request, 'profilegs/profilegs.html', {'user': user})
-    except Exception as e:
-        print(f"Erro ao renderizar o template: {e}")
-        raise Http404("Template não encontrado.")
+    return render(request, 'profilegs/profilegs.html', {
+        'user': user,  # Passando o usuário inteiro
+    })
